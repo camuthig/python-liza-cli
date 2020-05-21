73396e2a61e10c1d8b30326cdc9cab03c84d68e2
Chris Muthig <camuthig@gmail.com>
2020-05-20 20:48:35 -0700

    Add commands for watching repositories

--- liza_cli/bitbucket.py
+++ liza_cli/bitbucket.py
@@ -1,12 +1,29 @@
+import json
+
 import httpx
 
-endpoint = 'https://api.bitbucket.org/2.0'
+from dataclasses import dataclass
+from typing import Dict
 
 
-def test_token(username: str, token: str):
-    response = httpx.get(f'{endpoint}/user', auth=(username, token))
+@dataclass
+class BitBucket:
+    username: str
+    token: str
+    endpoint = 'https://api.bitbucket.org/2.0'
 
-    if response.status_code != 200:
-        return False
+    def _get(self, path: str):
+        return httpx.get(f'{self.endpoint}{path}', auth=(self.username, self.token))
 
-    return True
+    def test_token(self):
+        response = self._get('/user')
+
+        if response.status_code != 200:
+            return False
+
+        return True
+
+    def get_repository(self, workspace, name) -> Dict:
+        response = self._get(f'/repositories/{workspace}/{name}')
+
+        return json.loads(response.content)
--- liza_cli/liza.py
+++ liza_cli/liza.py
@@ -1,35 +1,68 @@
-import json
+from __future__ import annotations
+from typing import Dict, Optional
 from dataclasses import dataclass
+from datetime import datetime
 from pathlib import Path
-from typing import Dict
+from pydantic import BaseModel
 
 import typer
 
-from liza_cli import bitbucket
+from liza_cli.bitbucket import BitBucket
 
 app = typer.Typer()
 
 
+class Repository(BaseModel):
+    uuid: str
+    last_read: datetime
+    name: str
+
+
+class Config(BaseModel):
+    token: Optional[str]
+    username: Optional[str]
+    repositories: Dict[str, Repository]
+
+
 @dataclass
 class State:
+    client: Optional[BitBucket] = None
     config_file: Path = None
-    config: Dict = None
+    config: Config = None
 
 
 state = State()
 
 
+def not_logged_in():
+    typer.secho('You must configure credentials first.', fg=typer.colors.RED, err=True)
+    return 1
+
+
+def write_config():
+    with state.config_file.open('w') as f:
+        f.write(state.config.json(indent=4, sort_keys=True))
+
+
+def create_default_config():
+    state.config = Config(
+        repositories={},
+    )
+
+    write_config()
+
+
 @app.command()
 def credentials(username: str, token: str):
-    if not bitbucket.test_token(username, token):
+    client = BitBucket(username, token)
+    if not client.test_token():
         typer.secho('Invalid login credentials', fg=typer.colors.RED)
         return
 
-    state.config['token'] = token
-    state.config['username'] = username
+    state.config.token = token
+    state.config.username = username
 
-    with state.config_file.open('w') as f:
-        f.write(json.dumps(state.config, indent=4, sort_keys=True))
+    write_config()
 
     typer.secho('Token saved', fg=typer.colors.GREEN)
 
@@ -43,17 +76,65 @@ def reset():
         typer.secho('Reset all data.', fg=typer.colors.GREEN)
 
 
+@app.command()
+def watched():
+    for r in state.config.repositories.values():
+        typer.echo(r.name)
+
+
+@app.command()
+def watch(workspace: str, name: str):
+    if not state.client:
+        return not_logged_in()
+
+    repository = state.client.get_repository(workspace, name)
+
+    r = Repository(
+        name=repository['full_name'],
+        uuid=repository['uuid'],
+        last_read=datetime.now(),
+    )
+
+    if r.name in state.config.repositories.keys():
+        typer.secho(f'You are already watching {workspace}/{name}')
+        return
+
+    state.config.repositories[r.name] = r
+
+    write_config()
+
+    typer.secho(f'You are now watching {workspace}/{name}', fg=typer.colors.GREEN)
+
+
+@app.command()
+def unwatch(workspace: str, name: str):
+    if not state.client:
+        return not_logged_in()
+
+    full_name = f'{workspace}/{name}'
+
+    if full_name not in state.config.repositories.keys():
+        typer.secho(f'You are not watching {workspace}/{name}')
+        return
+
+    del state.config.repositories[full_name]
+
+    write_config()
+
+    typer.secho(f'You are no longer watching {workspace}/{name}', fg=typer.colors.GREEN)
+
+
 @app.callback()
 def main(config: Path = typer.Option(default=Path(Path.home(), '.liza'))):
-    if not config.exists():
-        with config.open('w') as f:
-            f.write("{}")
-
-    with config.open('r') as f:
-        content = f.read()
-
-    state.config = json.loads(content)
     state.config_file = config
+    if not config.exists():
+        create_default_config()
+
+    state.config = Config.parse_file(config.absolute())
+    state.config_file = config
+
+    if state.config.username and state.config.token:
+        state.client = BitBucket(state.config.username, state.config.token)
 
 
 if __name__ == "__main__":
